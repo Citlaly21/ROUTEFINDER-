2c17b9273a8f3dfcff62c7ab897b6ffd6bb1a9c7
Citlaly21 <cruzlisset87@gmail.com>
2024-06-19 13:25:27 -0600

    Actualización

--- components/reestablecerContraseña.js
+++ components/reestablecerContraseña.js
@@ -5,14 +5,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDRplYQcLl5p6G1fW4IBAwbWgYKMnri9eM",
-    authDomain: "route-finder-75a0b.firebaseapp.com",
-    databaseURL: "https://route-finder-75a0b-default-rtdb.firebaseio.com",
-    projectId: "route-finder-75a0b",
-    storageBucket: "route-finder-75a0b.appspot.com",
-    messagingSenderId: "833216771763",
-    appId: "1:833216771763:web:cbaec2a9392817195222c6",
-    measurementId: "G-HMMHJH8PPC"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
